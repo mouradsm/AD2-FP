2ecd119cafe9354c7024a8d7f3b9c1d094520b79
Diego Moura <diegosouza20049@hotmail.com>
2016-10-28 12:49:55 -0200

    Correção da Q2

--- Q2.py
+++ Q2.py
@@ -4,14 +4,11 @@ arquivoPesquisa     = input("Arquivo a ser pesquisado: ")
 def separaPalavras(arquivo):
     return [palavra for line in open(arquivo, 'r') for palavra in line.split()]
 
-def recuperaLinhas(arquivo):
-    conteudo = open(arquivo)
-    return conteudo.readlines()
-
 def busca_referencia(palavrasReferencia, arquivo):
-    linhas = recuperaLinhas(arquivo)
 
-    for indexLinha, line in enumerate(linhas):
+    conteudo = open(arquivo)
+
+    for indexLinha, line in enumerate(conteudo):
         for index, word in enumerate(line.strip('\n').split(" ")):
             if word in palavrasReferencia:
                 print('('+ '"' + word + '"' + ', ' + str(indexLinha + 1) + ', ' + str(index + 1) + ')')
