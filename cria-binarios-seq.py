63ffc09d508b3d974716549230759cd5a79ad6ac
Diego Moura <diegosouza20049@hotmail.com>
2016-10-28 14:32:11 -0200

    arquivo pra crirar binario seq

--- cria-binarios-seq.py
+++ cria-binarios-seq.py
@@ -1,9 +1,13 @@
 import random, struct
 
 try:
-    with open("arquivo.bin", "wb") as arq:
-        for i in range(11):
-            numero = struct.pack("i", i)
+    with open("arquivo-seq.bin", "wb") as arq:
+        for i in range(21):
+            numero = struct.pack("i", random.randint(0,100))
+            arq.write(numero)
+            numero = struct.pack("d", random.randrange(0,100))
+            arq.write(numero)
+            numero = struct.pack("d", random.randrange(0, 100))
             arq.write(numero)
     arq.close()
 except IOError:
