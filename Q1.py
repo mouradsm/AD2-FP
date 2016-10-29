2a81a184763797f04a9dcc4223ed742f8c47d7b4
Diego Moura <diegosouza20049@hotmail.com>
2016-10-28 22:08:23 -0200

    Letra a da questão 1 resolvida

--- Q1.py
+++ Q1.py
@@ -11,16 +11,18 @@ def cons(item, dados):
 
 def obterSetor(lista, inicio, fim):
 
-    if inicio > fim  or fim > len(lista):
-        return []
+    if fim > len(lista) or inicio > fim:
+      return []
 
     setores = []
 
-    def recusividade(lista, inicio,fim):
-
-        if(inicio <= fim):
-            setores.append(car(lista))
-            recusividade(cdr(lista), inicio + 1, fim)
+    def recusividade(lista, inicio, fim):
+        if not(len(lista) == 0 or fim < 1):
+            if inicio > 1:
+                recusividade(cdr(lista), inicio - 1, fim - 1)
+            elif fim > 1 or inicio <= 1:
+                setores.append(car(lista))
+                recusividade(cdr(lista), inicio, fim - 1)
 
     recusividade(lista, inicio, fim)
 
@@ -28,8 +30,8 @@ def obterSetor(lista, inicio, fim):
 
 print(obterSetor(["ana","maria", "chico", "igor", "juca"], 2, 4))
 
-print(obterSetor(["ana","maria", "chico", "igor", "juca"],4 , 1))
+print(obterSetor(["ana","maria", "chico", "igor", "juca"],4, 1))
 
-print(obterSetor([10, 2, 5, 13, 26, 4, 2, 9, 33, 18, 6, 99,12, 17], 5, 9))
+print(obterSetor([10, 2, 5, 13, 26, 4, 2, 9, 33, 18, 6, 99, 12, 17], 5, 9))
 
-print(obterSetor([10, 2, 5, 13, 26, 4, 2, 9, 33, 18, 6, 99,12, 17], 5, 19))
+print(obterSetor([10, 2, 5, 13, 26, 4, 2, 9, 33, 18, 6, 99, 12, 17], 5, 19))
